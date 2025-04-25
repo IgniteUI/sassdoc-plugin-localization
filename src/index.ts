5c25a191d70a7bd1b281f24918f64d7305a14cdd
Simeon Simeonoff <sim.simeonoff@gmail.com>
2025-04-25 11:48:19 +0300

    feat: add support for multiple languages

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import path from "path/posix";
 import { convert } from "./converter/convert.js";
 import { render } from "./renderer/render.js";
 
 type PluginOptions = {
   dir: string;
   mode: "import" | "export" | "both";
+  languages?: string[];
 };
 
 export default function (options: PluginOptions) {
@@ -11,20 +13,36 @@ export default function (options: PluginOptions) {
     beforeProcess: (ctx) => {
       const mode = options.mode || "both";
       const dir = options.dir;
+      const languages = options.languages || ["en"];
 
       if (mode === "export" || mode === "both") {
         console.info(`[↓] Exporting localization data to ${dir}`);
-        convert(ctx.data, dir);
+
+        for (const lang of languages) {
+          const langDir = path.join(dir, lang);
+          convert(ctx.data, langDir);
+        }
 
         if (mode === "export") {
-          console.info("[x] Terminating after export to allow for string modification");
+          console.info(
+            "[x] Terminating after export to allow for string modification",
+          );
           return true;
         }
       }
 
       if (mode === "import" || mode === "both") {
-        console.info(`[↑] Importing localization data from ${dir}`);
-        render(ctx.data, dir);
+        if (!ctx.lang || !options.languages?.includes(ctx.lang)) {
+          console.warn(
+            "[!] No valid language specified. Falling back to the default theme language.",
+          );
+          return false;
+        }
+
+        const langDir = path.join(dir, ctx.lang);
+
+        console.info(`[↑] Importing localization data from ${langDir}`);
+        render(ctx.data, langDir);
       }
 
       return false;
